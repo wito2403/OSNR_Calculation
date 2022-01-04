ffec45bcf74c88ad9d29ab94e407208ff740c1bf
wito2403 <jozwiak@hotmail.com>
2022-01-04 19:22:00 +0100

    added wavelength choice

--- OSNR_calculator.py
+++ OSNR_calculator.py
@@ -1,4 +1,4 @@
-from scipy.constants import h
+from scipy.constants import c, h
 from math import log10
 
 
@@ -9,7 +9,8 @@ def dBm2mW(dBm):
 
 class OSNRCalculator:
     def __init__(self, input_power, unit_attenuation, track_length, amplifiers_count,
-                 amplifier_locations: [], amplifier_gains: [], amplifier_noise_factors: []):
+                 amplifier_locations: [], amplifier_gains: [], amplifier_noise_factors: [],
+                 wavelength=1550, delta_wavelength=0.1):
         self.input_power = input_power
         self.unit_attenuation = unit_attenuation
         self.track_length = track_length
@@ -19,10 +20,20 @@ class OSNRCalculator:
         self.amplifier_gains = amplifier_gains
         self.amplifier_noise_factors = amplifier_noise_factors
 
-        self.constant = -58
+        self.wavelength = wavelength * 10 ** (-9)  # express in nanometers
+        self.delta_wavelength = delta_wavelength * 10 ** (-9)  # express in nanometers
+
+        self.constant = self.calculate_constant()
         self.fiber_sections = self.calculate_fiber_sections()
         self.output_signal_power = self.calculate_output_signal_power()
         self.amplifier_noise_powers = self.calculate_noise_powers()
+
+    def calculate_constant(self):
+        freq = c / self.wavelength
+        delta_freq = c * self.delta_wavelength / pow(self.wavelength, 2)
+        constant = 10 * log10(h * freq * delta_freq) + 30  # express in dBm
+        print(f"\n10log10(hfΔf):  {constant.__round__(2)} dBm\n")
+        return constant
 
     def calculate_output_signal_power(self):
         g = 0
@@ -40,13 +51,52 @@ class OSNRCalculator:
 
     def calculate_fiber_sections(self):
         fs = []
+        # num_zeros = 0
+        # for j in range(len(self.amplifier_locations)-1):
+        #     if j == 0:
+        #         if self.amplifier_locations[j] == float(0):
+        #             num_zeros += 1
+        #     else:
+        #         if self.amplifier_locations[j] == self.amplifier_locations[j+1]:
+        #             print("\n INKREMENTACJA NUM ZEROS")
+        #             num_zeros += 1
+        #             pass
 
-        for i in range(len(self.amplifier_locations)):
+
+
+        # if self.amplifier_locations[0] == float(0):
+        #     iterator = len(self.amplifier_locations) + 1
+        #     # print("float == 0")
+        # else:
+        #     iterator = len(self.amplifier_locations)
+        #     # print("cos innego")
+
+        # for i in range(len(self.amplifier_locations) + num_zeros):
+        #     if i == 0:
+        #         fs.append(self.amplifier_locations[i])
+        #
+        #     elif i == len(self.amplifier_locations):
+        #         fs.append(self.track_length - self.amplifier_locations[i - 1])
+        #     else:
+        #         fs.append(self.amplifier_locations[i] - self.amplifier_locations[i - 1])
+
+        for i in range(self.amplifiers_count):
             if i == 0:
-                fs.append(self.amplifier_locations[i])
-
+                if self.amplifier_locations[i] == float(0):
+                    fs.append(float(0))
+                else:
+                    fs.append(self.amplifier_locations[i])
             else:
                 fs.append(self.amplifier_locations[i] - self.amplifier_locations[i - 1])
+                if i == (len(self.amplifier_locations) - 1) and self.amplifier_locations[i] != self.track_length:
+
+                    fs.append(self.track_length - self.amplifier_locations[i])
+                    break
+
+                # if self.amplifier_locations[i] == self.amplifier_locations[i - 1]:
+                #     fs.append(float(0))
+                # else:
+        # print(fs)
         return fs
 
     def calculate_noise_powers(self):
@@ -54,8 +104,8 @@ class OSNRCalculator:
         for i in range(self.amplifiers_count):
             pni: float = self.amplifier_noise_factors[i] + sum(self.amplifier_gains[i:]) \
                          + self.constant - self.unit_attenuation * (sum(self.fiber_sections[i + 1:]))
-            print(f"Sum amplifier gains {i}: {sum(self.amplifier_gains[i:])}")
-            print(f"Sum fiber sections {i}: {sum(self.fiber_sections[i + 1:])}\n")
+            # print(f"Sum amplifier gains {i}: {sum(self.amplifier_gains[i:])}")
+            # print(f"Sum fiber sections {i}: {sum(self.fiber_sections[i + 1:])}\n")
             pn.append(pni)
         return pn
 
@@ -63,9 +113,10 @@ class OSNRCalculator:
         osnr: float
         # osnr = self.input_power
         output_power_mw = dBm2mW(self.output_signal_power)
-        print("Output signal power: ", self.output_signal_power)
-        print("PN1: ", self.amplifier_noise_powers[0])
-        print("PN2: ", self.amplifier_noise_powers[1])
+        print("Output signal power: ", self.output_signal_power.__round__(2))
+
+        for i in range(len(self.amplifier_noise_powers)):
+            print(f"PN{i+1}: {self.amplifier_noise_powers[i].__round__(2)}")
 
         pn_mw = []
         for i in self.amplifier_noise_powers:
--- main.py
+++ main.py
@@ -21,22 +21,24 @@ def input_amplifiers_data():
     amplifier_gains = []
     amplifier_noise_factors = []
 
+    wavelength = float(e_wavelength.get())
+    wavelength_delta = float(e_wavelength_delta.get())
     input_power = float(e_input_power.get())
     unit_attenuation = float(e_unit_attenuation.get())
     track_length = float(e_track_length.get())
     amplifiers_count = int(e_amplifiers_count.get())
 
-    tk.Label(root, text=f"Położenie wzmacniacza {1} [km]: ", pady=10, width=40).grid(row=10)
-    tk.Label(root, text=f"Wzmocnienie wzmacniacza G{1} [dB]: ", pady=10, width=40).grid(row=11)
-    tk.Label(root, text=f"Współczynnik szumów wzmacniacza N{1} [dB]: ", pady=10, width=40).grid(row=12)
+    tk.Label(root, text=f"Położenie wzmacniacza {1} [km]: ", pady=10, width=40).grid(row=12)
+    tk.Label(root, text=f"Wzmocnienie wzmacniacza G{1} [dB]: ", pady=10, width=40).grid(row=13)
+    tk.Label(root, text=f"Współczynnik szumów wzmacniacza N{1} [dB]: ", pady=10, width=40).grid(row=14)
 
     e_amplifier_location = tk.Entry(root, width=50)
     e_amplifier_gain = tk.Entry(root, width=50)
     e_amplifier_noise_factor = tk.Entry(root, width=50)
 
-    e_amplifier_location.grid(row=10, column=1)
-    e_amplifier_gain.grid(row=11, column=1)
-    e_amplifier_noise_factor.grid(row=12, column=1)
+    e_amplifier_location.grid(row=12, column=1)
+    e_amplifier_gain.grid(row=13, column=1)
+    e_amplifier_noise_factor.grid(row=14, column=1)
 
     def append_data():
         amplifier_locations.append(float(e_amplifier_location.get()))
@@ -52,16 +54,16 @@ def input_amplifiers_data():
         append_data()
         ctr += 1
 
-        tk.Label(root, text=f"Położenie wzmacniacza {ctr} [km]: ", pady=10, width=40).grid(row=10)
-        tk.Label(root, text=f"Wzmocnienie wzmacniacza G{ctr} [dB]: ", pady=10, width=40).grid(row=11)
-        tk.Label(root, text=f"Współczynnik szumów wzmacniacza N{ctr} [dB]: ", pady=10, width=40).grid(row=12)
+        tk.Label(root, text=f"Położenie wzmacniacza {ctr} [km]: ", pady=10, width=40).grid(row=12)
+        tk.Label(root, text=f"Wzmocnienie wzmacniacza G{ctr} [dB]: ", pady=10, width=40).grid(row=13)
+        tk.Label(root, text=f"Współczynnik szumów wzmacniacza N{ctr} [dB]: ", pady=10, width=40).grid(row=14)
 
         if ctr == amplifiers_count:
             b1.grid_forget()
-            tk.Button(root, command=calculate_osnr, text="Oblicz OSNR").grid(row=13, column=1)
+            tk.Button(root, command=calculate_osnr, text="Oblicz OSNR").grid(row=15, column=1)
 
     b1 = tk.Button(root, command=next_amplifier, text="Następny wzmacniacz")
-    b1.grid(row=13, column=1)
+    b1.grid(row=15, column=1)
 
     def calculate_osnr():
         append_data()
@@ -83,13 +85,14 @@ def input_amplifiers_data():
         # amplifiers_count = 2
 
         calculator = OSNRCalculator(input_power, unit_attenuation, track_length, amplifiers_count,
-                                    amplifier_locations, amplifier_gains, amplifier_noise_factors)
+                                    amplifier_locations, amplifier_gains, amplifier_noise_factors,
+                                    wavelength, wavelength_delta)
 
         print(f"Fiber sections:  {calculator.fiber_sections}")
 
         OSNR = calculator.calculate_osnr()
-        tk.Label(root, text=f"OSNR: ", pady=30, width=40).grid(row=14, column=0)
-        tk.Label(root, text=f"{OSNR.__round__(2)} [dB]", pady=30, width=40).grid(row=14, column=1)
+        tk.Label(root, text=f"OSNR: ", pady=30, width=40).grid(row=16, column=0)
+        tk.Label(root, text=f"{OSNR.__round__(2)} [dB]", pady=30, width=40).grid(row=16, column=1)
         pass
 
 
@@ -100,22 +103,29 @@ def restart():
 
 root = tk.Tk()
 root.title('[SIS] Program obliczający OSNR na wyjściu toru optycznego')
-root.geometry("800x430")
+root.geometry("800x550")
 
-tk.Label(root, text="Moc sygnału na wejściu światłowodu [dBm]: ", pady=10, width=40).grid(row=0)
-tk.Label(root, text="Tłumienność jednostkowa światłowodu [dB/km]: ", pady=10, width=40).grid(row=1)
-tk.Label(root, text="Całkowita długość toru [km]: ", pady=10, width=40).grid(row=2)
-tk.Label(root, text="Liczba wzmacniaczy optycznych: ", pady=10, width=40).grid(row=3)
+tk.Label(root, text="Długość fali λ [nm]: ", pady=10, width=40).grid(row=0)
+tk.Label(root, text="Szerokość filtra optycznego Δλ [nm]: ", pady=10, width=40).grid(row=1)
+pe = u'P\u2099'  # low index n
+tk.Label(root, text=f"Moc nadajnika {str(pe)} [dBm]: ", pady=10, width=40).grid(row=2)
+tk.Label(root, text="Tłumienność jednostkowa α [dB/km]: ", pady=10, width=40).grid(row=3)
+tk.Label(root, text="Całkowita długość toru [km]: ", pady=10, width=40).grid(row=4)
+tk.Label(root, text="Liczba wzmacniaczy optycznych: ", pady=10, width=40).grid(row=5)
 
+e_wavelength = tk.Entry(root, width=50)
+e_wavelength_delta = tk.Entry(root, width=50)
 e_input_power = tk.Entry(root, width=50)
 e_unit_attenuation = tk.Entry(root, width=50)
 e_track_length = tk.Entry(root, width=50)
 e_amplifiers_count = tk.Entry(root, width=50)
 
-e_input_power.grid(row=0, column=1)
-e_unit_attenuation.grid(row=1, column=1)
-e_track_length.grid(row=2, column=1)
-e_amplifiers_count.grid(row=3, column=1)
+e_wavelength.grid(row=0, column=1)
+e_wavelength_delta.grid(row=1, column=1)
+e_input_power.grid(row=2, column=1)
+e_unit_attenuation.grid(row=3, column=1)
+e_track_length.grid(row=4, column=1)
+e_amplifiers_count.grid(row=5, column=1)
 
 tk.Button(root,
           text='Zamknij program!',
@@ -131,7 +141,7 @@ tk.Button(root,
 #                                 padx=80)
 
 tk.Button(root,
-          text='Rozpocznij wprowadzanie danych wzmacniaczy', command=input_amplifiers_data).grid(row=4,
+          text='Rozpocznij wprowadzanie danych wzmacniaczy', command=input_amplifiers_data).grid(row=6,
                                                                                                  column=1,
                                                                                                  sticky=tk.W,
                                                                                                  pady=20)
